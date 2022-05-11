e73df651dbb8501a3fbe2e5681902090030d5b1b
SmartleDev <100072559+SmartleDev@users.noreply.github.com>
2022-05-11 14:01:56 +0530

    Add files via upload

--- build/controller/parentController.js
+++ build/controller/parentController.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllParents = void 0;
+exports.updateParent = exports.setParentInfo = exports.getAllParents = void 0;
 const config_1 = __importDefault(require("../config/config"));
 exports.getAllParents = ((req, res) => {
     // db.getConnection(function (err: any, conn: any){
@@ -18,4 +18,32 @@ exports.getAllParents = ((req, res) => {
     });
     //     conn.release();
     // })   
+});
+exports.setParentInfo = ((req, res) => {
+    const { parent_id } = req.body;
+    if (parent_id) {
+        config_1.default.query('SELECT * FROM smartle.parent WHERE parent_id=?', [parent_id], (err, rows) => {
+            if (err) {
+                console.log(err);
+            }
+            res.send({ rows });
+        });
+    }
+    else {
+        res.send({ result: "failed" });
+    }
+});
+exports.updateParent = ((req, res) => {
+    const { phone, parent_id } = req.body;
+    if (phone) {
+        config_1.default.query('UPDATE smartle.parent SET parent_contactno = ? WHERE parent_id = ?', [phone, parent_id], (err, rows) => {
+            if (err) {
+                console.log(err);
+            }
+            res.send({ result: "success" });
+        });
+    }
+    else {
+        res.send({ result: "failed" });
+    }
 });
